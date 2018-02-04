d56fedf5124a397d665561b602bb78054cc916cf
David Siegel <dasiege@microsoft.com>
2018-02-04 11:27:42 -0800

    Work on README

--- README.md
+++ README.md
@@ -1,31 +1,37 @@
-# Awesome Typed Data
+# Awesome Typed Datasets [![Awesome](https://cdn.rawgit.com/sindresorhus/awesome/d7305f38d29fed78fa85652e3a63e154dd8e8829/media/badge.svg)](https://github.com/sindresorhus/awesome)
 
-The following are public JSON datasets that have been strongly
+These are public JSON datasets that have been strongly
 typed with [quicktype](https://github.com/quicktype/quicktype).
 Each is a repo with code in Go, C++, Objective-C, Java, TypeScript, Swift, Elm, JSON Schema, and C# for
 reading and writing the JSON produced by these APIs.
 
 
-### Blockchain
+## Blockchain
 
 * [Blockchain Bitcoin APIs](https://github.com/typeguard/types-blockchain) (blockchain.info)
 
-### Finance
+## Finance
 
 * [Consumer Complaint Database](https://github.com/typeguard/types-consumer-complaints) (catalog.data.gov/dataset/consumer-complaint-database)
 
-### Media
+## Media
 
 * [Reddit](https://github.com/typeguard/types-reddit) (reddit.com)
 
-### Science
+## Science
 
 * [USGS Earthquake Data](https://github.com/typeguard/types-earthquakes) (earthquake.usgs.gov)
 
-### Space
+## Space
 
 * [Open APIs From Space](https://github.com/typeguard/types-astronauts-in-space) (open-notify.org)
 
-### Technology
+## Technology
 
 * [GitHub API](https://github.com/typeguard/types-github) (api.github.com)
+
+## Contributing
+If you want to contribute, please read the [contribution guidelines](CONTRIBUTING.md).
+
+## License
+[![CC0](http://mirrors.creativecommons.org/presskit/buttons/88x31/svg/cc-zero.svg)](https://creativecommons.org/publicdomain/zero/1.0/)
--- src/generate.ts
+++ src/generate.ts
@@ -106,7 +106,7 @@ function readme(data: DatasetMeta[]): string {
 
   function* categoryList(name: string, category: DatasetMeta[]) {
     yield ``;
-    yield `### ${name}`;
+    yield `## ${name}`;
     yield ``;
 
     for (const meta of category) {
@@ -122,7 +122,7 @@ function readme(data: DatasetMeta[]): string {
   }
 
   function* generate() {
-    yield `# Awesome Typed Data`;
+    yield `# Awesome Typed Datasets [![Awesome](https://cdn.rawgit.com/sindresorhus/awesome/d7305f38d29fed78fa85652e3a63e154dd8e8829/media/badge.svg)](https://github.com/sindresorhus/awesome)`;
 
     const displayNames = languages
       .map(l => l.displayName)
@@ -132,15 +132,26 @@ function readme(data: DatasetMeta[]): string {
 
     yield* [
       ``,
-      `The following are public JSON datasets that have been strongly`,
+      `These are public JSON datasets that have been strongly`,
       `typed with [quicktype](https://github.com/quicktype/quicktype).`,
       `Each is a repo with code in ${nameList} for`,
       `reading and writing the JSON produced by these APIs.`,
       ``
     ];
+
     for (const name of Object.keys(categories).sort()) {
       yield* categoryList(name, categories[name]);
     }
+
+    yield* [
+      ``,
+      `## Contributing`,
+      `If you want to contribute, please read the [contribution guidelines](CONTRIBUTING.md).`,
+      ``,
+      `## License`,
+      `[![CC0](http://mirrors.creativecommons.org/presskit/buttons/88x31/svg/cc-zero.svg)](https://creativecommons.org/publicdomain/zero/1.0/)`,
+      ``
+    ];
   }
 
   return Array.from(generate()).join("\n");
